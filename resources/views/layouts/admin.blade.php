434c91aafdc9bcd53d08c166cf8b9e6beda6a7b3
tianzhaogao <tianzhaogao@aprbrother.com>
2017-12-11 23:05:28 +0800

    up

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -45,7 +45,7 @@
             <h3><i class="fa fa-fw fa-clipboard"></i>广告管理</h3>
             <ul class="sub_menu">
                 <li><a href="{{url('admin/ad/create')}}" ><i class="fa fa-fw fa-plus-square"></i>添加广告</a></li>
-                <li><a href="{{url('admin/ad').'?type=1'}}"><i class="fa fa-fw fa-list-ul"></i>广告列表</a></li>
+                <li><a href="{{url('admin/ad')}}"><i class="fa fa-fw fa-list-ul"></i>广告列表</a></li>
             </ul>
         </li>
         <li>
